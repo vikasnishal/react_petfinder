2506e84a9bbb893b028ae99ddad16f6e1bcaf1fa
vikas kumar <vikas@simplotel.com>
2018-11-04 13:19:32 +0530

    adding config files

--- src/Details.js
+++ src/Details.js
@@ -12,7 +12,7 @@ const petfinder = pf({
 class Details extends React.Component {
   state = {
     loading: true,
-    showModal: true
+    showModal: false
   };
   toggleModal = () => {
     this.setState({
