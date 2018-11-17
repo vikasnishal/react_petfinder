2e272478c9245fdc4366b547fd888238cb40cd69
vikas kumar <vikas@simplotel.com>
2018-11-17 11:47:16 +0530

    Updates

---
+++ precache-manifest.4b0dd5df65b4857acebab0b19d91a6fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e0ef0ac8ec37aa35462",
+    "url": "/static/js/main.9e0ef0ac.chunk.js"
+  },
+  {
+    "revision": "61548689cdb88fb98c34",
+    "url": "/static/js/1.61548689.chunk.js"
+  },
+  {
+    "revision": "9e0ef0ac8ec37aa35462",
+    "url": "/static/css/main.fc588afb.chunk.css"
+  },
+  {
+    "revision": "d0c21e600579b8f2ca78b9f7823151c6",
+    "url": "/index.html"
+  }
+];
