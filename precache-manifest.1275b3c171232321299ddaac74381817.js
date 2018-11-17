19d40269f036e61c7fd44ecf59dfda38753970b2
vikas kumar <vikas@simplotel.com>
2018-11-17 11:30:49 +0530

    Updates

---
+++ precache-manifest.1275b3c171232321299ddaac74381817.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bba194c38d7ccc33af4",
+    "url": "/btholt/complete-intro-to-react-v4/static/js/runtime~main.8bba194c.js"
+  },
+  {
+    "revision": "64808231386b61b8307d",
+    "url": "/btholt/complete-intro-to-react-v4/static/js/main.64808231.chunk.js"
+  },
+  {
+    "revision": "61548689cdb88fb98c34",
+    "url": "/btholt/complete-intro-to-react-v4/static/js/1.61548689.chunk.js"
+  },
+  {
+    "revision": "9e6491cf8d28c342ecbc0aa13a660f75",
+    "url": "/btholt/complete-intro-to-react-v4/index.html"
+  }
+];
