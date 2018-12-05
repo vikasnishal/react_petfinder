9b22cefae2fbb58c679eed37237585d033ee69d3
vikas kumar <vikas@simplotel.com>
2018-12-05 18:05:48 +0530

    Updates

---
+++ precache-manifest.02f30af64dab632bbb17069bb5e1d6e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dd2543674d1d7230fc8",
+    "url": "/react_petfinder/static/js/runtime~main.0dd25436.js"
+  },
+  {
+    "revision": "b4300443d34b4894aa45",
+    "url": "/react_petfinder/static/js/main.b4300443.chunk.js"
+  },
+  {
+    "revision": "90b767c215e6451e0ddc",
+    "url": "/react_petfinder/static/js/1.90b767c2.chunk.js"
+  },
+  {
+    "revision": "b4300443d34b4894aa45",
+    "url": "/react_petfinder/static/css/main.fc588afb.chunk.css"
+  },
+  {
+    "revision": "2500a1b3f6902b58958f457d7d0d9020",
+    "url": "/react_petfinder/index.html"
+  }
+];
